0c96137ee09ac400b3656e49fe76e1f157789772
Andrey Chausenko <Andrey.Chausenko@team.telstra.com>
2016-05-09 12:46:36 +1000

    Added TypeScript interface definitions

---
+++ lib/index.d.ts
@@ -0,0 +1,16 @@
+export class GrokPattern {
+    parse(str: string, callback: { (err: any, result: any) });
+    parseSync(str: string): any;
+}
+
+export class GrokCollection {
+    createPattern(expression: string, id?: string): GrokPattern;
+    getPattern(id: string): GrokPattern;
+    load(filePath: string, callback: { (err?: any) });
+    loadSync(filePath: string): number;
+    count(): number;
+}
+
+export function loadDefault(loadModulesOrCallback?: string[] | { (err: any, collection: GrokCollection) }, callback?: { (err: any, collection: GrokCollection) });
+
+export function loadDefaultSync(loadModules?: string[]): GrokCollection;
